279754b66a4fb655c37e92540c58ac49a515a93d
Jefry <jnavass@est.ups.edu.ec>
2021-07-14 11:44:57 -0500

    Completado todas las peticiones

--- server.js
+++ server.js
@@ -15,7 +15,8 @@ app.use(require("./server/routes/usuario"));
 mongoose.connect('mongodb://localhost:27017/Cafe', {
     useCreateIndex: true,
     useNewUrlParser: true,
-    useUnifiedTopology: true
+    useUnifiedTopology: true,
+    useFindAndModify: false
 }, (err, res) => {
     if (err) throw err;
     console.log("Base de datos online!");
--- server/routes/usuario.js
+++ server/routes/usuario.js
@@ -2,11 +2,62 @@ const express = require("express");
 const app = express();
 const Usuario = require("../models/usuario");
 const bcrypt = require('bcrypt');
+const underscore = require('underscore');
+
 
 app.get('/usuario', (req, res) => {
-    res.json("get Usuario");
+    let desde = req.query.desde || 0;
+    desde = Number(desde);
+    let limite = req.query.limite || 5;
+    limite = Number(limite);
+
+    Usuario.find({ estado: true }).skip(desde).limit(limite).exec((err, usuarios) => {
+        if (err) {
+            return res.status(400).json({
+                ok: false,
+                err: err.message
+            })
+        }
+        Usuario.countDocuments({ estado: true }, (err, conteo) => {
+            res.json({
+                ok: true,
+                numero: conteo,
+                usuarios
+            })
+        });
+
+    })
 });
-app.put('/usuario', function(req, res) {
+
+
+app.put('/usuario/:id', function(req, res) {
+    let id = req.params.id;
+    let body = underscore.pick(req.body, ['nombre', 'email', 'img', 'role', 'estado']);
+
+
+
+    Usuario.findByIdAndUpdate(id, body, { new: true, runValidators: true, context: 'query' }, (err, usuarioDB) => {
+        if (err) {
+            return res.status(400).json({
+                ok: false,
+                err: err.message
+            })
+        }
+        if (!usuarioDB) {
+            return res.status(400).json({
+                ok: false,
+                err: "Usuario no encontrado"
+            })
+        }
+
+        res.json({
+            ok: true,
+            usuario: usuarioDB
+        })
+    });
+});
+app.post('/usuario', (req, res) => {
+
     let body = req.body;
     let usuario = new Usuario({
         nombre: body.nombre,
@@ -29,11 +80,24 @@ app.put('/usuario', function(req, res) {
 
     })
 });
-app.post('/usuario', (req, res) => {
-    res.json("post Usuario");
-});
-app.delete('/usuario', (req, res) => {
-    res.json("delete Usuario");
+app.delete('/usuario/:id', (req, res) => {
+    let id = req.params.id;
+    let desactivar = {
+        estado: false
+    }
+
+    Usuario.findByIdAndUpdate(id, desactivar, { new: true, runValidators: true, context: 'query' }, (err, usuarioDB) => {
+        if (err) {
+            return res.status(400).json({
+                ok: false,
+                err: err.message
+            })
+        }
+        res.json({
+            ok: true,
+            usuario: usuarioDB
+        })
+    });
 });
 
 module.exports = app;
